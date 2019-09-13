6106b05b576de3b4898c60b9b8e7cc440e7d1153
Tuhin <tuhintowhidul9@gmail.com>
2019-09-13 11:11:10 +0600

    #3 - Data & Methods

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 new Vue({
-    el:'#vue-app',
-    data:{
-        name:'Tuhin'
+    el: '#vue-app',
+    data: {
+        name: 'Tuhin',
+        job: 'Engineer'
+    },
+    methods: {
+        greet:function (time) {
+            return 'Good '+time+' '+this.name;
+        }
     }
 });
