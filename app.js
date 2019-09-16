7d34ddf2f3e4aac1bd442f458fee6f58d8e61a59
Tuhin <tuhintowhidul9@gmail.com>
2019-09-16 19:45:00 +0600

    #7 - Keyboard Events

--- app.js
+++ app.js
@@ -1,25 +1,16 @@
 new Vue({
     el: '#vue-app',
     data: {
-        age:25,
-        x:0,
-        y:0
+        
     },
     methods: {
-       add:function (inc) {
-           this.age+=inc;
-       },
-       sub:function(dec){
-           this.age-=dec;
-       },
-       updateXY:function(event){
-            // console.log(event);
-            this.x=event.offsetX;
-            this.y=event.offsetY;
+        logName:function(){
+            console.log('name');
+        },
 
-       },
-       click:function(){
-           alert('clicked');
-       }
+        logAge:function(){
+            console.log('Age');
+        }
+       
     }
 });
