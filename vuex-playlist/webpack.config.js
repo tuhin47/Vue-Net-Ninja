a4318abc43bbed4217e6991d25a4e364100ffde4
Md. Towhidul Islam <tuhintowhidul9@gmail.com>
2020-01-17 23:17:49 +0600

    2. A simple vue app

--- vuex-playlist/webpack.config.js
+++ vuex-playlist/webpack.config.js
@@ -11,12 +11,6 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.css$/,
-        use: [
-          'vue-style-loader',
-          'css-loader'
-        ],
-      },      {
         test: /\.vue$/,
         loader: 'vue-loader',
         options: {
@@ -42,13 +36,11 @@ module.exports = {
   resolve: {
     alias: {
       'vue$': 'vue/dist/vue.esm.js'
-    },
-    extensions: ['*', '.js', '.vue', '.json']
+    }
   },
   devServer: {
     historyApiFallback: true,
-    noInfo: true,
-    overlay: true
+    noInfo: true
   },
   performance: {
     hints: false
