6a932037a2b8d2fba38dd0a594f6c1d9db942213
MD. Towhidul Islam <tuhintowhidul9@gmail.com>
2020-01-21 12:00:59 +0600

    #19 - Nesting Components

---
+++ myvue/src/Ninjas.vue
@@ -0,0 +1,18 @@
+<template>
+    <ul>
+        <li v-for="ninja in ninjas" :key="ninja">{{ ninja }}</li>
+    </ul>
+</template>
+
+<script>
+export default {
+    data () {
+        return {
+          ninjas: ['Yoshi', 'Mario', 'Ryu']
+        }
+    }
+}
+</script>
+
+<style>
+</style>
